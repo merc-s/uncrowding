a8d218f28e2f40ffa238e664f567629a3019db33
Merkourios Simos <112860681+merc-s@users.noreply.github.com>
2022-11-11 17:45:55 +0100

    Add files via upload

---
+++ bash_script_uncrowding_V4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --chdir /scratch/simos
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 16
+#SBATCH --mem 128G
+#SBATCH --time 24:00:00
+echo STARTING AT `date`
+python /home/simos/uncrowding/uncrowding_pipeline_uncrowding_V4.py
+echo FINISHED AT `date`
